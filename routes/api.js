75b484a26b5883d0df14e113dff2d970207d816a
GedSni <ged.snicorius@gmail.com>
2017-11-05 15:45:58 +0200

    Fixed bugs in multiple functions, added function for friend adding

--- routes/api.js
+++ routes/api.js
@@ -15,8 +15,7 @@ router.get('/users', function(req, res, next){
 
 //*******BETTER TO USE Q OR BLUEBIRD PROMISES (or async)*******
 //GET ONE method for USER (returns all user's friends and that user's info)
-//DO THIS FIRST PRIORITY! -- JWT -- GIT -- PORT
-/*router.get('/users/:id/friendlist', function(req, res, next){
+router.get('/users/:id/friendlist', function(req, res, next){
 	var result = {};
 	User.findOne({_id: req.params.id}).then(function(user){
 		if(!user)
@@ -35,10 +34,10 @@ router.get('/users', function(req, res, next){
 			}).catch(next);
 		}
 	}).catch(next);
-});*/
+});
 
 //GET ONE method for USERS (returns one user's data)
-router.get('/users/:id/friendlist/add', function(req, res, next){
+router.get('/users/:id', function(req, res, next){
 	User.findOne({_id: req.params.id}).then(function(user){
 		if(!user)
 			res.status(404).send({error: "The requested resource could not be found (special case)"});
@@ -57,25 +56,47 @@ router.post('/users', function(req, res, next){
 
 //*******BETTER TO USE Q OR BLUEBIRD PROMISES (or async)*******
 //PUT method for USER (adds a user to a friendlist (both ways))
-router.put('/users/:id/friends', function(req, res, next){
-	var result = {};
-	User.update({_id: req.params.id}).then(function(user){
-		if(!user)
-			res.status(404).send({error: "The requested resource could not be found (special case)"});
-		else
-		{
-			result.user = user;
-			User.find({friend: req.params.id}).then(function(friend){
-				if(!friend.length)
-					res.send(result);
-					else
-					{
-						result.friend = friend;
-						res.send(result);
+router.put('/users/:id/friendlist/add', function(req, res, next){
+	if(req.params.id != req.body.friend){
+		User.findOne({_id: req.params.id}).then(function(user){
+			if(!user)
+				res.status(404).send({error: "The requested resource could not be found (special case)"});
+			else{
+				User.update(
+					{ _id: req.params.id},
+					{ $push: {friend: req.body.friend}}
+				).then(function(err){
+					if(err.nModified == 0){
+						res.status(404).send({error: "Nothing was modified, something went wrong."});
+						return;
 					}
-			}).catch(next);
-		}
-	}).catch(next);
+					User.findOne({_id: req.body.friend}).then(function(user2){
+						if(!user2)
+							res.status(404).send({error: "The requested resource could not be found (special case)"});
+						else
+							User.update(
+								{ _id: req.body.friend},
+								{ $push: {friend: req.params.id}}
+							).then(function(err){
+								if(err.nModified == 0){
+									res.status(404).send({error: "Nothing was modified, something went wrong."});
+									return;
+								}
+								User.findOne({_id: req.params.id}).then(function(user3){
+									if(!user3)
+										res.status(404).send({error: "The requested resource could not be found (special case)"});
+									else
+										res.send(user3);
+								});
+							});
+					});
+				});
+			}
+		}).catch(next);
+	}
+	else{
+		res.status(404).send({error: "Trying to add yourself as a friend :("});
+	}
 });
 
 //PUT method for USERS
